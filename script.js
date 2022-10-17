564cd564616cc7f5e2adbd63b9e04f26db4c3068
me-luc <lukasalmeida372@gmail.com>
2022-10-16 22:07:51 -0300

    update

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ function select(button) {
 	}
 
 	button.id = "selected";
+
+	ableButton();
 }
 
-function submit() {}
+function submit() {
+	const hidedElements = document.getElementById("hide");
+	hidedElements.id = "";
+
+	const visibleElements = document.querySelector(".square1-elements");
+	visibleElements.id = "hide";
+
+	const rating = document.getElementById("selected");
+
+	document.querySelector(".star").innerHTML = rating.innerHTML;
+}
+
+function ableButton() {
+	document.querySelector(".submit").removeAttribute("disabled");
+}
